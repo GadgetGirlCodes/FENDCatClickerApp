9862aa30f9c8584da3a2162d2f5fee33abda0564
Rebekah Lane <gadgetgirlmommy@gmail.com>
2018-09-14 23:37:28 -0500

    feat: Added functions for additional cat image

--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
-const kittyCat = document.querySelector('.kitty');
-const clickyWords = document.querySelector('.clickyWords');
-let numOf = 0;
+const kittyCatOne = document.querySelector('#one');
+const kittyCatTwo = document.querySelector('#two');
+const clickyWordsOne = document.querySelector('.clickyWordsOne');
+const clickyWordsTwo = document.querySelector('.clickyWordsTwo');
+let numOfOne = 0;
+let numOfTwo = 0;
 
-function clickKitty() {
-	numOf++;
-	console.log(numOf);
+function clickKittyOne() {
+	numOfOne++;
+	console.log("kitty one: " + numOfOne);
 }
 
-function numOfClicks() {
-	clickyWords.innerText = "You've clicked the kitty " + numOf + " times!";
+function clickKittyTwo() {
+	numOfTwo++;
+	console.log("kitty two: " + numOfTwo);
 }
 
-kittyCat.addEventListener('click', () => {
-	clickKitty();
-	numOfClicks();
+function numOfClicksOne() {
+	clickyWordsOne.innerText = "You've clicked this kitty " + numOfOne + " times!";
+}
+
+function numOfClicksTwo() {
+	clickyWordsTwo.innerText = "You've clicked this kitty " + numOfTwo + " times!";
+}
+
+kittyCatOne.addEventListener('click', () => {
+	clickKittyOne();
+	numOfClicksOne();
+});
+
+kittyCatTwo.addEventListener('click', () => {
+	clickKittyTwo();
+	numOfClicksTwo();
 });
